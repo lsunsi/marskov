859b2cf57ca91a724d87a409fd3b77a517cec25b
lsunsi <sunsi.lucas@gmail.com>
2017-11-30 21:39:25 -0200

    addes first policy: the greedy one

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 mod policy;
+mod policies;
---
+++ src/policies/greedy.rs
@@ -0,0 +1,38 @@
+use policy::Policy;
+
+#[derive(Default)]
+struct Greedy;
+
+impl Policy for Greedy {
+  fn choose<'a, A>(&mut self, action_values: &'a [(A, f64)]) -> Option<&'a A> {
+      action_values
+        .iter()
+        .max_by(|av1, av2| av1.1.partial_cmp(&av2.1).unwrap())
+        .map(|av| &av.0)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq)]
+    enum Action {Jump, Stay}
+
+    #[test]
+    fn none_for_empty_action_values() {
+        let mut greedy = Greedy::default();
+        assert_eq!(greedy.choose(&[]) as Option<&Action>, None);
+    }
+
+    #[test]
+    fn some_max_valued_action() {
+        let mut greedy = Greedy::default();
+
+        let action_values_1 = [(Action::Jump, 0.1), (Action::Stay, 0.2)];
+        let action_values_2 = [(Action::Jump, 0.2), (Action::Stay, 0.1)];
+
+        assert_eq!(greedy.choose(&action_values_1), Some(&Action::Stay));
+        assert_eq!(greedy.choose(&action_values_2), Some(&Action::Jump));
+    }
+}
